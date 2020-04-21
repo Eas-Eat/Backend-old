25a64eb028c4e796fbe69919ee2fe78e83d479aa
Dylan DE SOUSA <dds1991@hotmail.fr>
2020-04-21 15:16:49 +0200

    Fix: Cannot find module './models/User

--- tests/server.test.ts
+++
@@ -1,9 +0,0 @@
-import { createTestClient } from 'apollo-server-testing'
-import { server } from '../src/server'
-
-const { query } = createTestClient(server)
-describe('Apollo Server', () => {
-  afterAll(async () => {
-    await server.stop()
-  })
-})
