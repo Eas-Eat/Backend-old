034886c6c81ab3567777dc19c67a1e705e2bbe55
MaximeG2018 <maxime.gouenard@gmail.com>
2020-04-27 16:54:33 +0200

    Add requete GetAllCuisines

--- src/schema/models/cuisine.ts
+++ src/schema/models/cuisine.ts
@@ -1,9 +1,29 @@
-import { objectType } from '@nexus/schema'
+import { objectType, extendType } from '@nexus/schema'
 
 export const Cuisine = objectType({
   name: 'Cuisine',
   definition(t): void {
     t.model.type()
     t.model.svgName()
+  },
+})
+
+export const getAllCuisines = extendType({
+  type: 'Query',
+  definition(t): void {
+    t.list.field('getAllCuisines', {
+      type: 'Cuisine',
+      resolve: async (_: unknown, {}, ctx: GraphQlContext) => {
+        try {
+          const { prisma } = ctx
+          const cuisines = await prisma.cuisine.findMany()
+
+          return cuisines
+        } catch (error) {
+          console.log(error)
+          throw new Error(error.code)
+        }
+      },
+    })
   },
 })
